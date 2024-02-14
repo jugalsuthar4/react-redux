066d05eb65362b6eb97f3efbd20076b844e44ee9
Jugal D Suthar <jugalsutharpro@gmail.com>
2024-02-14 11:00:51 +0530

    removed unused imports

--- src/components/cakeContainer.tsx
+++ src/components/cakeContainer.tsx
@@ -1,23 +1,24 @@
-import React from "react"
-import { buyCake } from "../actions/cake"
+import { buyCake } from "../actions/cake";
 import { connect } from "react-redux";
-const CakeContainer=({numOfCakes,buyCake}:any)=>{
-    return <>
-        <h2>cake container</h2>
-        <p>{numOfCakes}</p>
-        <button onClick={buyCake}>buy cake</button>
-     </>
-}
+const CakeContainer = ({ numOfCakes, buyCake }: any) => {
+  return (
+    <>
+      <h2>cake container</h2>
+      <p>{numOfCakes}</p>
+      <button onClick={buyCake}>buy cake</button>
+    </>
+  );
+};
 
-const mapStateToProps=(state:any)=>{
-    return {
-        numOfCakes:state.numOfCakes
-    }
-}
+const mapStateToProps = (state: any) => {
+  return {
+    numOfCakes: state.numOfCakes,
+  };
+};
 
-const mapDispatchToProps=(dispatch:any)=>{
-    return {
-        buyCake:()=>dispatch(buyCake())
-    }
-}
-export default connect(mapStateToProps,mapDispatchToProps)(CakeContainer);
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
